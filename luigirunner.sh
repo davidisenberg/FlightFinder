737175e27f089e7703986a93250b1d9e1f935919
David Isenberg <david.isenberg@gmail.com>
2019-06-15 18:06:13 -0400

    luigirunner add

--- luigirunner.sh
+++ luigirunner.sh
@@ -1,4 +1,6 @@
-
+export WORKON_HOME=~/.virtualenvs
+VIRTUALENVWRAPPER_PYTHON='/usr/bin/python3'
+source /usr/local/bin/virtualenvwrapper.sh
 workon myvirtualenv
-export PYTHONPATH="/home/davidisenberg/FlightFinder/tasks:/home/davidisenberg/FlightFinder/services"
+export PYTHONPATH="/home/davidisenberg/FlightFinder/tasks:/home/davidisenberg/FlightFinder/services:/home/davidisenberg/FlightFinder"
 luigi --module flight_task LoadAllFlights --local-scheduler
