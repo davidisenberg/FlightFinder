187e0de7c02a9003b38d68e24bff3797c18412f7
David Isenberg <david.isenberg@gmail.com>
2019-06-23 13:19:42 -0400

    test performance

--- services/vendor/skypicker.py
+++ services/vendor/skypicker.py
@@ -23,13 +23,46 @@ class SkyPickerApi:
 
     def call_api(self,url):
         try:
-            #fo = open("testdata.obj", 'rb')
-            #data = pickle.load(fo)
+
             data = requests.get(url).text
 
-            #fileObject = open("testdata.obj", 'wb')
-            #pickle.dump(data, fileObject)
-            #fileObject.close()
+            sp_flights = json.loads(data)["data"]
+
+            # flights = []
+            # for sp_flight in sp_flights:
+            #     flights.append({'FlyFrom': sp_flight["flyFrom"],
+            #                     'FlyTo': sp_flight["flyTo"],
+            #                     'Price': sp_flight["price"],
+            #                     'Airline': sp_flight["airlines"][0],
+            #                     'Duration': sp_flight["fly_duration"],
+            #                     'ArrivalTimeUTC': sp_flight["aTimeUTC"],
+            #                     'DepartTimeUTC': sp_flight["dTimeUTC"],
+            #                     'FlightNum': sp_flight["route"][0]["flight_no"],
+            #                     'DataDate': int(datetime.date.today().strftime('%Y%m%d'))})
+            #
+            # df = pd.DataFrame(flights)
+            # if len(df) == 0:
+            #     return pd.DataFrame()
+            df = pd.DataFrame()
+            df["DataDate"] = 1
+            df["FlyFrom"] = 2
+            df["FlyTo"] = 3
+
+        except Exception as e:
+            print(e)
+            raise
+
+        return pd.DataFrame()
+
+    def call_api_orig(self, url):
+        try:
+            # fo = open("testdata.obj", 'rb')
+            # data = pickle.load(fo)
+            data = requests.get(url).text
+
+            # fileObject = open("testdata.obj", 'wb')
+            # pickle.dump(data, fileObject)
+            # fileObject.close()
 
             sp_flights = json.loads(data)["data"]
 
@@ -57,6 +90,8 @@ class SkyPickerApi:
             raise
 
         return pd.DataFrame()
+
+
 
 
 #SkyPickerApi().get_flights("JFK","LHR",datetime.date.today(), datetime.date.today() + datetime.timedelta(days=2))
