be662ccb385554e29b9f64e523daf29b2311934f
David Isenberg <david.isenberg@gmail.com>
2019-06-23 12:58:55 -0400

    test performance

--- tasks/flight_task.py
+++ tasks/flight_task.py
@@ -38,7 +38,7 @@ class LoadAllFlights(luigi.Task):
        return luigi.LocalTarget(path)
 
     def run(self):
-        directs = DirectService().get_directs_list()[:6]
+        directs = DirectService().get_directs_list()[:24]
         today = datetime.date.today()
 
         FLY_FROM = 0
