bfd06f1c3732a453bb805d399344be421d6f35c9
David Isenberg <david.isenberg@gmail.com>
2019-06-29 18:40:21 -0400

    add react bundle

---
+++ ui/build/precache-manifest.c1369f47430f8ced350f70ebef291a95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "efb0870a1d5124fdd3fb",
+    "url": "/static/js/main.d8cc54eb.chunk.js"
+  },
+  {
+    "revision": "effc858b3766e2887bb2",
+    "url": "/static/js/2.4439659f.chunk.js"
+  },
+  {
+    "revision": "efb0870a1d5124fdd3fb",
+    "url": "/static/css/main.f7fb00e2.chunk.css"
+  },
+  {
+    "revision": "effc858b3766e2887bb2",
+    "url": "/static/css/2.c68fdfa7.chunk.css"
+  },
+  {
+    "revision": "d8ea09308265a49b4939fdef1b74fb17",
+    "url": "/index.html"
+  }
+];
