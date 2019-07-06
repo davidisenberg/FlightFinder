af8868d4dd1c6c688780688d9860f2459cbe2b5c
David Isenberg <david.isenberg@gmail.com>
2019-07-06 18:10:25 -0400

    make actual recos work with real data, improving performance

---
+++ precache-manifest.d1e8f2652e2e4efc00bbf9ebd23d9b5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "415227e7cd833454a6ad",
+    "url": "./static/js/main.5cb98745.chunk.js"
+  },
+  {
+    "revision": "a1f5476e8419534be90b",
+    "url": "./static/js/2.9969fd5b.chunk.js"
+  },
+  {
+    "revision": "415227e7cd833454a6ad",
+    "url": "./static/css/main.0303b37a.chunk.css"
+  },
+  {
+    "revision": "a1f5476e8419534be90b",
+    "url": "./static/css/2.0c4cb9d7.chunk.css"
+  },
+  {
+    "revision": "0fee1ef2e03d1cd4c8fbec96c95ffc6b",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7db39fe162ec897b37f2d61bb00706aa.js"
+  "./precache-manifest.d1e8f2652e2e4efc00bbf9ebd23d9b5e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
