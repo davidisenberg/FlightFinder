9925a189fff89827af402e25151702d34324b2bf
David Isenberg <david.isenberg@gmail.com>
2019-07-06 18:55:25 -0400

    get ui working

---
+++ ui/build/precache-manifest.7db39fe162ec897b37f2d61bb00706aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "25aca2b53e5b3087067c",
+    "url": "/static/js/main.53644b4a.chunk.js"
+  },
+  {
+    "revision": "a1f5476e8419534be90b",
+    "url": "/static/js/2.9969fd5b.chunk.js"
+  },
+  {
+    "revision": "25aca2b53e5b3087067c",
+    "url": "/static/css/main.0303b37a.chunk.css"
+  },
+  {
+    "revision": "a1f5476e8419534be90b",
+    "url": "/static/css/2.0c4cb9d7.chunk.css"
+  },
+  {
+    "revision": "c2740287cf5e7d902d4bc48d156ccf4d",
+    "url": "/index.html"
+  }
+];
