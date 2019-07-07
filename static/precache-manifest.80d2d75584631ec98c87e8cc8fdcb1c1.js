d1658dc95f64ac57f38f2138cdae255795a6ae0d
David Isenberg <david.isenberg@gmail.com>
2019-07-07 09:50:53 -0400

    devops for building the ui

---
+++ static/precache-manifest.80d2d75584631ec98c87e8cc8fdcb1c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "14268535cb910c34c174",
+    "url": "/static/js/main.673f8063.chunk.js"
+  },
+  {
+    "revision": "04c206b63b23a52a1059",
+    "url": "/static/js/2.8e7dbd66.chunk.js"
+  },
+  {
+    "revision": "14268535cb910c34c174",
+    "url": "/static/css/main.0303b37a.chunk.css"
+  },
+  {
+    "revision": "04c206b63b23a52a1059",
+    "url": "/static/css/2.0c4cb9d7.chunk.css"
+  },
+  {
+    "revision": "28729ca43db545c465247cf0a80b53b1",
+    "url": "/index.html"
+  }
+];
