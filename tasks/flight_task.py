f9b9bb22aaade5a6a4562dc56c9bf60972db8114
David Isenberg <david.isenberg@gmail.com>
2019-07-14 19:31:34 -0400

    luigi task killed due to too much memory - change to regular parquet file

--- tasks/flight_task.py
+++ tasks/flight_task.py
@@ -93,25 +93,25 @@ class CleanUp(luigi.Task):
 
     def run(self):
 
-        # for the_file in os.listdir(self.__partial_files):
-        #     file_path = os.path.join(self.__partial_files, the_file)
-        #     try:
-        #         if os.path.isfile(file_path):
-        #             os.unlink(file_path)
-        #         elif os.path.isdir(file_path):
-        #             shutil.rmtree(file_path)
-        #     except Exception as e:
-        #         print(e)
-        #
-        # for the_file in os.listdir(self.__local_target):
-        #     if the_file.startswith("20"):
-        #         continue
-        #     file_path = os.path.join(self.__local_target, the_file)
-        #     try:
-        #         if os.path.isfile(file_path):
-        #             os.unlink(file_path)
-        #     except Exception as e:
-        #         print(e)
+        for the_file in os.listdir(self.__partial_files):
+            file_path = os.path.join(self.__partial_files, the_file)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print(e)
+
+        for the_file in os.listdir(self.__local_target):
+            if the_file.startswith("20"):
+                continue
+            file_path = os.path.join(self.__local_target, the_file)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+            except Exception as e:
+                print(e)
 
         with self.output().open('w') as f:
             f.write('Yep, done for day... and what!')
